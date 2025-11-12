4c707d6de062de080cb59a710b356b161aef686e
Arnela <kezo.arnela@gmail.com>
2025-11-12 01:06:47 +0100

    initial commit

---
+++ CafeHub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CafeHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
