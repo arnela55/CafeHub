19773063f358944a2a6aa01a2d540090bce5a490
Arnela <kezo.arnela@gmail.com>
2025-11-20 22:22:17 +0100

    dodana skica menija i controller

---
+++ CafeHub/Controllers/MenuController.cs
@@ -0,0 +1,14 @@
+﻿using CafeHub.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CafeHub.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = DatabaseModel.Products;
+            return View(products);
+        }
+    }
+}
---
+++ CafeHub/Models/DatabaseModel.cs
@@ -0,0 +1,19 @@
+﻿namespace CafeHub.Models
+{
+    public class DatabaseModel
+    {
+        public static List<User> Users = new List<User>();
+
+        public static List<Product> Products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Espresso", Category = "Kafa", Price = 2.50M, Description="Jaka crna kafa", Image=null },
+            new Product { Id = 2, Name = "Cappuccino", Category = "Kafa", Price = 3.00M, Description="Espresso sa pjenom od mlijeka", Image=null },
+            new Product { Id = 3, Name = "Sendvič", Category = "Hrana", Price = 4.00M, Description="Razni topli sendvici", Image=null }
+        };
+
+        public static List<Order> Orders = new List<Order>();
+        public static List<OrderItem> OrderItems = new List<OrderItem>();
+        public static List<Reservation> Reservations = new List<Reservation>();
+        public static List<Payment> Payments = new List<Payment>();
+    }
+}
