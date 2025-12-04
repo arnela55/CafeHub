387e51941f4979c5d345828703b2d07dc68afba3
Arnela <kezo.arnela@gmail.com>
2025-12-04 16:28:45 +0100

    implemented gallery

---
+++ CafeHub/Controllers/GalleryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CafeHub.Controllers
+{
+    public class GalleryController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<string> images = new List<string>
+            {
+                "/images/gallery/interior.jpg"
+            };
+
+            return View(images);
+        }
+    }
+}
